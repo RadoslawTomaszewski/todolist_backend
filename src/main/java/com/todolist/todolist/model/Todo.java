612cb7a4e35c064b7e9deb32ce2f5351d54b3e2f
radek44413 <radek4441399@gmail.com>
2023-06-15 13:05:23 +0200

    datetimelocal changes format

--- src/main/java/com/todolist/todolist/model/Todo.java
+++ src/main/java/com/todolist/todolist/model/Todo.java
@@ -1,5 +1,6 @@
 package com.todolist.todolist.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ public class Todo {
     private Long id;
     private boolean isCompleted;
     private String name;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime deadline;
 
 
@@ -26,6 +28,7 @@ public class Todo {
     public String getName() {
         return name;
     }
+
     public LocalDateTime getDeadline() {return deadline;}
 
 

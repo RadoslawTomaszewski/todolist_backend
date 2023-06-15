8dbf755a1f798f7140b0f059b0148f1cd5591b57
radek44413 <radek4441399@gmail.com>
2023-06-15 12:12:53 +0200

    CORS policy edit 2

--- src/main/java/com/todolist/todolist/WebConfig.java
+++ src/main/java/com/todolist/todolist/WebConfig.java
@@ -4,14 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://radoslawtomaszewski.github.io")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        registry.addMapping("/**");
     }
 }
--- src/main/java/com/todolist/todolist/controller/TodoController.java
+++ src/main/java/com/todolist/todolist/controller/TodoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://radoslawtomaszewski.github.io")
+@CrossOrigin("*")
 public class TodoController {
 
 
